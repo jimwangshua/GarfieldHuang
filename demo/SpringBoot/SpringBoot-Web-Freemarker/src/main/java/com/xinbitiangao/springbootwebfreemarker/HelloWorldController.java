5c3ac872673751604713b88050f65053e22a99aa
GarfieldHuang <38367970+GarfieldHuang@users.noreply.github.com>
2018-11-22 13:41:58 +0800

    更新SpringBoot-Web-Freemarker

---
+++ demo/SpringBoot/SpringBoot-Web-Freemarker/src/main/java/com/xinbitiangao/springbootwebfreemarker/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.xinbitiangao.springbootwebfreemarker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+public class HelloWorldController {
+
+    private String message = "Hello World";
+
+    @GetMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "HelloWorld";
+    }
+
+}
