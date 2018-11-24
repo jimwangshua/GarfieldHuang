5d66fb3ebe5f4546d6f3f5098657c262d4cd47b9
huang <82988601@qq.com>
2018-11-24 11:19:15 +0800

    webjars

---
+++ demo/SpringBoot/SpringBoot-Web-Webjars/src/main/java/com/xinbitiangao/springbootwebwebjars/SpringbootwebwebjarsApplication.java
@@ -0,0 +1,14 @@
+package com.xinbitiangao.springbootwebwebjars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootwebwebjarsApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebwebjarsApplication.class, args);
+    }
+}
